992d05425a81f651585fc8810ac012301e718326
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-12-02 19:04:13 +0300

    add coocies

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import VueCookies from 'vue-cookies';
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.css';
 import BootstrapVue from 'bootstrap-vue';
 
+Vue.use(VueCookies);
 Vue.config.productionTip = false
 Vue.use(BootstrapVue);
 
